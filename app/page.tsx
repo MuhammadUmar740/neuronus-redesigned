df403a0e0dd167428a2fa187bc35c2a6527d4a70
MuhammadUmar740 <umer.slysol@gmail.com>
2026-01-30 12:36:06 +0500

    feat: Neuronus protects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CheckValue from "@/components/home/CheckValue";
 import DataSecurity from "@/components/home/data-security";
 import HeaderHome from "@/components/home/HeaderHome";
+import NeuronusProtects from "@/components/home/neuronus-protects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeaderHome />
       <CheckValue />
       <DataSecurity />
+      <NeuronusProtects />
     </>
   );
 }
