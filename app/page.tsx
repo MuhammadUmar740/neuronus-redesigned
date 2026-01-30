674235a4a94c3752dd8ab21192a3440692aec0dc
HamzaSlysol <hamza.slysol@gmail.com>
2026-01-30 12:31:28 +0500

    conflicts removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
 import CheckValue from "@/components/home/CheckValue";
-<<<<<<< HEAD
-import HeaderCard from "@/components/home/HeaderCard";
 import AccessNetwork from "@/components/homePage/AccessNetwork";
-import Curtains from "@/public/images/Curtains";
-import dots from "@/public/images/dots-landing-header.png";
-import Image from "next/image";
-=======
 import DataSecurity from "@/components/home/data-security";
 import HeaderHome from "@/components/home/HeaderHome";
->>>>>>> 6254f03ab767f352e6fa52e04a657e828b327292
 
 export default function Home() {
   return (
     <>
       <HeaderHome />
       <CheckValue />
-<<<<<<< HEAD
-      <AccessNetwork />
-=======
       <DataSecurity />
->>>>>>> 6254f03ab767f352e6fa52e04a657e828b327292
+      <AccessNetwork />
     </>
   );
 }
