4777c6561b1bc89abeef574c281c53aff6348490
MuhammadUmar740 <umer.slysol@gmail.com>
2026-02-04 14:51:49 +0500

    feat: Next gen encryption section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeaderHome from "@/components/home/HeaderHome";
 import NeuronusProtects from "@/components/home/neuronus-protects";
 import AccessNetwork from "@/components/home/access-network/AccessNetwork";
 import BasicRules from "@/components/home/basic-rules";
+import NextGenEncryption from "@/components/home/next-gen-encryption";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <NeuronusProtects />
       <AccessNetwork />
       <BasicRules />
+      <NextGenEncryption />
     </>
   );
 }
