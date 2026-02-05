82a43bd770ec603ce305db91cc113dbabc9d2049
HamzaSlysol <hamza.slysol@gmail.com>
2026-02-05 12:29:22 +0500

    fix: page title

--- components/home/workspace/WorkSpace.tsx
+++ components/home/workspace/WorkSpace.tsx
@@ -18,10 +18,10 @@ const WorkSpace = () => {
   return (
     <div className="pt-10 lg:pt-0 pb-16 md:pb-24 -mt-5">
       <div className=" bg-white max-w-[95vw] relative rounded-tr-[62px] rounded-br-[62px] py-12">
-        <Heading className="text-center mb-3 ">Communication</Heading>
+        <Heading className="text-center mb-3 ">Workspace</Heading>
         <Description
           className="text-center mb-10 md:mb-16"
-          text="Information exchange without witnesses.   "
+          text="A cloud office that doesn’t look over your shoulder."
         />
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 lg:gap-4">
           <div className="flex flex-col lg:justify-between mt-8 gap-14 lg:gap-0 pe-8">
